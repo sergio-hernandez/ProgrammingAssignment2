0c1095e893616542966641603128cdc3a41bdde3
Sergio Hernandez <sergio_nandez@hotmail.com>
2016-03-23 21:23:48 -0500

    Programming Assignment 2 commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions are intended to cache the inverse
+## of a matrix, compute it and print it to the
+## console. This is useful to avoid repeatedly and
+## costly computations of matrices inverse.
 
-## Write a short comment describing this function
+## This function creates a list containing the
+## functions which cache the inverse. It also warns
+## stops the execution and provides an error message
+## communicating the user if the matrix isn't a
+## squared matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(smatrix1=matrix()){
+        if(dim(smatrix1)[1]!=dim(smatrix1)[2]){
+                stop("smatrix1 (first argument) not a squared matrix")
+        }
+        inverse<-NULL
+        set_values<-function(nmatrix1=smatrix1){
+                smatrix1<<-nmatrix1
+                inverse<<-NULL
+        }
+        get_smatrix1<-function() smatrix1
+        setinverse<-function(solvedinverse) inverse <<- solvedinverse
+        getinverse<-function() inverse
+        list(set_values = set_values,
+             get_smatrix1 = get_smatrix1,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function evaluates if the inverse already exists
+## and if it isn't NULL. If so, it retrieves the inverse
+## calculated before. If it hasn't been calculated, it
+## calculates the inverse, caches it and print it to the
+## console.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(smatrix1,...){
+        inverse<-my_inverse$getinverse()
+        if(!is.null(inverse)){
+                message("getting cached data")
+                return(inverse)
+        }
+        data_smatrix1 <- my_inverse$get_smatrix1()
+        inverse <- solve(data_smatrix1,...)
+        my_inverse$setinverse(inverse)
+        inverse
 }
